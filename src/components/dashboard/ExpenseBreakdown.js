83e9b85ce8259d8421fd8c9bdb348ddd65f6fd79
Nazifa Tasnim Shifa <nazifatasnimshifa@gmail.com>
2026-01-22 16:18:42 +0600

    Create ExpenseBreakdown.js

---
+++ src/components/dashboard/ExpenseBreakdown.js
@@ -0,0 +1,39 @@
+// src/components/dashboard/ExpenseBreakdown.js
+
+import { Pie } from 'react-chartjs-2';
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { position: 'right' },
+      title: {
+        display: true,
+        text: 'Top Expense Categories',
+      },
+    },
+  };
+
+  const data = {
+    labels: ['Financial', 'Admin & General', 'Selling & Marketing'],
+    datasets: [
+      {
+        label: 'Amount (in ৳)',
+        data: [3567499, 1893527, 95000],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+export default function ExpenseBreakdown() {
+    return (
+        <div className="h-64 rounded-lg bg-white p-4 shadow">
+            <Pie data={data} options={options} />
+        </div>
+    )
+}
