cc7cdb8587b61d9db4ce838cb7eb4bd13c425824
Nazifa Tasnim Shifa <nazifatasnimshifa@gmail.com>
2026-01-26 08:30:42 +0600

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
+  // We hardcode the URL here to bypass the "Missing environment variable" error
   datasource: {
-    url: env("DATABASE_URL"),
+    url: "mysql://u448110646_ars_admin:v5jL>3JG;Hl~J58c!OFS@srv992.hstgr.io:3306/u448110646_ars_group",
   },
 });
